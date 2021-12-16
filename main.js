0183c8cd70850aefa1b9bfe4fe6328db52ac1e51
bunga29 <bungafairuzzz@gmail.com>
2021-12-16 19:37:34 +0700

    commit ubah ukuran dan lain lain

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-"use strict";
+//"use strict";
 
 var blocks = [],
-    width = 400,
+    width = 700,
     height = 400,
-    ROWS = 10,
-    COLS = 8,
-    blockWidth = 50,
-    blockHeight = 10,
+    ROWS = 5, //jumlah baris blok
+    COLS = 10, //jumlah kolom blok
+    blockWidth = 100, //lebar blok
+    blockHeight = 20, //tinggi blok
     renderer = null,
     scene = null,
     camera = null;
@@ -41,19 +41,19 @@ start();
 function initEdges() {
     var sidegeometry = new THREE.BoxGeometry(1, 400, 100);
     var sidematerial = new THREE.MeshPhongMaterial(
-	{color:0x00FF00,// 0x2222aa,
+	{color: 0x00FF00,
 	 specular: 0x333333,
 	 shininess: 3}
     );
     var leftbox = new THREE.Mesh(sidegeometry, sidematerial);
-    var topGeometry = new THREE.BoxGeometry(400, 1, 100);
+    var topGeometry = new THREE.BoxGeometry(1000, 1, 100);
     var rightbox = new THREE.Mesh(sidegeometry, sidematerial);
-    var backbox = new THREE.Mesh(new THREE.BoxGeometry(400, 400, 1),
+    var backbox = new THREE.Mesh(new THREE.BoxGeometry(1000, 400, 1),
 				 sidematerial);
     var botbox = new THREE.Mesh(topGeometry, sidematerial);
     var topbox = new THREE.Mesh(topGeometry, sidematerial);
-    leftbox.position.set(-1, 200, 0);
-    rightbox.position.set(width, 200, 0);
+    leftbox.position.set(-300, 200, 0);
+    rightbox.position.set(700, 200, 0);
     backbox.position.set(200, 200, -49);
     botbox.position.set(200, 0, 0);
     topbox.position.set(200, 400, 0);
@@ -76,10 +76,13 @@ function initStatusText() {
     var status = document.createElement("div");
     status.id = "status";
     status.style.position = "absolute";
-    status.style.top = "0px";
-    status.style.padding = "10px";
-    status.style.width = "200px";
-    status.style.height = "100px";
+    
+    status.style.top = "50px";
+    status.style.left = "100px";
+    status.style.fontSize = "24px";
+    // status.style.padding = "10px";
+    // status.style.width = "200px";
+    // status.style.height = "100px";
     status.style.fontFamily = "monospace";
     status.innerHTML = "Score: " + game.score + " Lives: " + game.lives;
     status.style.color = "white";
@@ -90,9 +93,22 @@ function drawText(msg) {
     var text = document.createElement("div");
     text.id = "message";
     text.style.position = "absolute";
-    text.style.top = window.innerHeight/2 + "px";
+    text.style.top = 300 + "px";
     text.style.fontSize = 48;
-    text.style.left = window.innerWidth/3 + "px";
+    text.style.left = 600 + "px";
+    text.innerHTML = msg;
+    text.style.color = "white";
+    text.style.fontFamily = "arial";
+    document.body.appendChild(text);
+}
+
+function drawTextLastScore(msg) {
+    var text = document.createElement("div");
+    text.id = "last-score";
+    text.style.position = "absolute";
+    text.style.top = 400 + "px";
+    text.style.fontSize = 48;
+    text.style.left = 600 + "px";
     text.innerHTML = msg;
     text.style.color = "white";
     text.style.fontFamily = "arial";
@@ -115,30 +131,32 @@ function start() {
     initKeys();
     var time = 0;
     function mainLoop(timestamp) {
-	requestAnimationFrame(mainLoop);
-	renderer.render(scene, camera);
-	var delta = timestamp - time;
-	time = timestamp;
-	var paddleDelta = paddle.speed*delta/1000;
-    	if ((paddle.dir === -1 && paddle.x > paddleDelta) ||
-    	    (paddle.dir === 1 && paddle.x + paddleDelta + paddle.width < width))
-    	    paddle.x += paddleDelta * paddle.dir;
-	if (game.state === "running") {
-	    ball.x += ball.velocity.x * delta / 1000;
-    	    ball.y += ball.velocity.y * delta / 1000;
-    	    detectCollisions();
-	} else  {
-    	    ball.x = paddle.x+paddle.width/2;
-    	    ball.y = paddle.y+paddle.height+ball.radius;
-    	} 
-	if (ball) {
-	    ball.mesh.position.x = ball.x;
-	    ball.mesh.position.y = ball.y;
-	}
-	if (paddle) {
-	    paddle.mesh.position.x = paddle.x+paddle.width/2
-	    paddle.mesh.position.y = paddle.y+paddle.height/2;
-	}
+        requestAnimationFrame(mainLoop);
+        renderer.render(scene, camera);
+        var delta = timestamp - time;
+        time = timestamp;
+        var paddleDelta = paddle.speed*delta/1000;
+        //perpindahan posisi paddle
+        if ((paddle.dir === -1 && paddle.x > paddleDelta-300) ||
+            (paddle.dir === 1 && paddle.x + paddleDelta + paddle.width < width))
+            paddle.x += paddleDelta * paddle.dir;
+        //perpindahan posisi ball
+        if (game.state === "running") {
+            ball.x += ball.velocity.x * delta / 1000;
+            ball.y += ball.velocity.y * delta / 1000;
+            detectCollisions();
+        } else  {
+            ball.x = paddle.x+paddle.width/2;
+            ball.y = paddle.y+paddle.height+ball.radius;
+        } 
+        if (ball) {
+            ball.mesh.position.x = ball.x;
+            ball.mesh.position.y = ball.y;
+        }
+        if (paddle) {
+            paddle.mesh.position.x = paddle.x+paddle.width/2
+            paddle.mesh.position.y = paddle.y+paddle.height/2;
+        }
     }
     mainLoop();
 }
@@ -146,14 +164,18 @@ function start() {
 function updateStatus() {
     var s = document.getElementById("status");
     if (game.lives === 0) {
-	drawText("GAME OVER");
-	resetGame();
-	game.state = "over";
+        var stat = document.getElementById("status");
+        stat.parentNode.removeChild(stat);
+        drawText("GAME OVER");
+        drawTextLastScore("Your Score : " + game.score);
+        resetGame();
+        game.state = "over";
     } else if (game.blockCount === 0) {
-	drawText("YOU WIN");
-	resetGame();
-	game.state = "over";
+        drawText("YOU WIN");
+        resetGame();
+        game.state = "over";
     } 
+    
     s.innerHTML = 'Score: ' + game.score + ' Lives: ' + game.lives;
 }
 
@@ -164,27 +186,27 @@ function resetGame() {
     scene.remove(paddle.mesh);
     scene.remove(ball.mesh);
     for (var i = 0; i < ROWS; i++)
-	for (var j = 0; j < COLS; j++)
-	    scene.remove(blocks[i][j].object);
+        for (var j = 0; j < COLS; j++)
+            scene.remove(blocks[i][j].object);
     initGame();
 }
 
 function detectCollisions() {
     // walls
-    if (ball.x+ball.radius > width) {
-	ball.velocity.x = -ball.velocity.x;
-	ball.x = width-ball.radius;
-	return;
+    if (ball.x+ball.radius > width) {  //wall kanan
+        ball.velocity.x = -ball.velocity.x;
+        ball.x = width-ball.radius;
+        return;
     }
-    if (ball.x-ball.radius < 0) {
-	ball.velocity.x = -ball.velocity.x;
-	ball.x = ball.radius;
-	return;
+    if (ball.x-ball.radius < -300) { //wall kiri
+        ball.velocity.x = -ball.velocity.x;
+        ball.x = -300 + ball.radius;
+        return;
     }
-    if (ball.y+ball.radius > height) {
-	ball.velocity.y = -ball.velocity.y;
-	ball.y = height-ball.radius;
-	return;
+    if (ball.y+ball.radius > height) { //wall atas
+        ball.velocity.y = -ball.velocity.y;
+        ball.y = height-ball.radius;
+        return;
     }
     
     // paddle
@@ -206,21 +228,22 @@ function detectCollisions() {
     
     // blocks
     if (ball.y+ball.radius < height-ROWS*blockHeight)
-	return;
+	    return; 
     var col = Math.floor((ball.x-ball.radius)/blockWidth);
     var row = Math.floor((height-ball.y-ball.radius)/blockHeight);
-    if (row < 0 || col < 0 || blocks[row][col].status === 1)
-	return;
+    console.log(col, row);
+    if (row < 0 || col < -3 || blocks[row][col].status === 1)
+	    return;
     var x = col*blockWidth;
     var y = height-row*blockHeight;
     if (ball.x+ball.radius >= x && ball.x-ball.radius < x+blockWidth
 	&& ball.y+ball.radius > y-blockHeight && ball.y-ball.radius < y) {
-	ball.velocity.y = -ball.velocity.y;
-	blocks[row][col].status = 1;
-	scene.remove(blocks[row][col].object);
-	game.score++;
-	game.blockCount--;
-	updateStatus();
+        ball.velocity.y = -ball.velocity.y;
+        blocks[row][col].status = 1;
+        scene.remove(blocks[row][col].object);
+        game.score++;
+        game.blockCount--;
+        updateStatus();
     }
 }
 
@@ -230,13 +253,7 @@ function initKeys() {
 	    paddle.dir = 1;
 	else if (e.keyCode === 37 || e.keyCode === 65)
 	    paddle.dir = -1;
-	else if (e.keyCode === 32 && game.state === "ready") {
-	    ball.velocity.x += paddle.dir * 25;
-	    if (ball.velocity.x > width)
-		ball.velocity.x = width;
-	    game.state = "running";
-	}
-    else if (e.keyCode === 87 && game.state === "ready") {
+	else if ((e.keyCode === 32 || e.keyCode === 87) && game.state === "ready") {
 	    ball.velocity.x += paddle.dir * 25;
 	    if (ball.velocity.x > width)
 		ball.velocity.x = width;
@@ -249,10 +266,8 @@ function initKeys() {
 	}
     }, false);
     document.addEventListener("keyup", function(e) {
-	if ((e.keyCode === 39 && paddle.dir === 1)||
-        (e.keyCode === 68 && paddle.dir === 1)||
-        (e.keyCode === 65 && paddle.dir === -1)||
-	    (e.keyCode === 37 && paddle.dir === -1))
+	if (((e.keyCode === 39 || e.keyCode === 68) && paddle.dir === 1) ||
+	    ((e.keyCode === 37 || e.keyCode === 65) && paddle.dir === -1))
     	    paddle.dir = 0;
     }, false);
 }
@@ -274,25 +289,26 @@ function resetPaddle() {
 function initGame() {
     var geometry = new THREE.BoxGeometry(blockWidth, blockHeight, 100);
     for (var i = 0; i < ROWS; i++) {
-	blocks[i] = [];
-	for (var j = 0; j < COLS; j++) {
-	    var material = new THREE.MeshPhongMaterial(
-		{color: new THREE.Color(randColor(),
-					randColor(),
-					randColor()),
-        specular: 0x333333,
-        shininess: 1
-		});
-	    var object = new THREE.Mesh(geometry, material);
-	    blocks[i][j] = { status: 0,
-			     object: object
-			   };
-	    object.position.set(j*blockWidth+blockWidth/2,
-				height-(i*blockHeight+blockHeight/2), 0);
-	    scene.add(object);
-	}
+        blocks[i] = [];
+        for (var j = -3; j < COLS-3; j++) {
+            var material = new THREE.MeshPhongMaterial(
+            {color: new THREE.Color(randColor(),
+                        randColor(),
+                        randColor()),
+
+             specular: 0x333333,
+             shininess: 1
+            });
+            var object = new THREE.Mesh(geometry, material);
+            blocks[i][j] = { status: 0,
+                    object: object
+                };
+            object.position.set(j*blockWidth+blockWidth/2,
+                    height-(i*blockHeight+blockHeight/2), 0);
+            scene.add(object);
+        }
     }
-    var material = new THREE.MeshPhongMaterial({color: 0xFFFF00});
+    var material = new THREE.MeshPhongMaterial({color: 0xF0F8FF});
     var paddleGeometry = new THREE.BoxGeometry(paddle.width, paddle.height, 40);
     paddle.mesh = new THREE.Mesh(paddleGeometry, material);
     scene.add(paddle.mesh);
@@ -306,5 +322,7 @@ function initGame() {
 }
 
 function randColor() {
-    return 0.2 + Math.random();
+    return 0.3 + 0.7*Math.random();
 }
+
+
